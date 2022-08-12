c82aad2c3d73ee8994d24c1d086fe8c57e8026bf
AndriySt92 <andriyua-1992@mail.ru>
2022-08-12 18:47:27 +0300

    Add all components and pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,46 @@
-import './App.css';
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import { Contacts } from './pages/Contacts/Contacts'
+import { Home } from './pages/Home/Home'
+import { RepairWork } from './pages/RepairWork/RepairWork'
+import { Services } from './pages/Services/Services'
+import { Layout } from './components/Layout/Layout'
+import { FacadeWorks } from './pages/FacadeWorks/FacadeWorks'
+import { PlasterWorks } from './pages/PlasterWorks/PlasterWorks'
+import { RoofingWorks } from './pages/RoofingWorks/RoofingWorks'
+import { ProjectDevelopment } from './pages/ProjectDevelopment/ProjectDevelopment'
+import { ConcreteWorks } from './pages/ConcreteWorks/ConcreteWorks'
+import { PavingWorks } from './pages/PavingWorks/PavingWorks'
+import { DecorativePlaster } from './pages/DecorativePlaster/DecorativePlaster'
+import { Portfolio } from './pages/Portfolio/Portfolio'
+import { Tips } from './pages/Tips/Tips'
+import { Project } from './pages/Project/Project'
+import { Tip } from './pages/Tip/Tip'
 
-function App() {
+function App() { 
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home />} />
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="services" element={<Services />} />
+          <Route path="repairWorks" element={<RepairWork />} />
+          <Route path="facadeWorks" element={<FacadeWorks  />} />
+          <Route path="plasterWorks" element={<PlasterWorks />} />
+          <Route path="roofingWorks" element={<RoofingWorks  />} />
+          <Route path="projectDevelopment" element={<ProjectDevelopment  />} />
+          <Route path="concreteWorks" element={<ConcreteWorks  />} />
+          <Route path="pavingWorks" element={<PavingWorks  />} />
+          <Route path="decorativePlaster" element={<DecorativePlaster  />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="tips" element={<Tips />} />
+          <Route path="tip/:urlTitle" element={<Tip />} />
+          <Route path="project/:name" element={<Project />} />
+        </Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import "lightgallery.js/dist/css/lightgallery.css";
+import "react-image-gallery/styles/scss/image-gallery.scss";
+import './index.scss'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+)
