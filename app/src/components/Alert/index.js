04b3b7a9a7ef16be6287a6d654fcbcda768bafe4
AndriySt92 <andriyua-1992@mail.ru>
2024-05-14 15:40:26 +0300

    Fixed Alert component

--- app/src/components/Alert/index.js
+++ app/src/components/Alert/index.js
@@ -6,12 +6,12 @@ export const Alert = ({ type }) => {
     <div className={`alert ${type === 'success' ? 'alert-success' : 'alert-danger'}`} role="alert">
       {type === 'success' ? (
         <>
-          <h5 class="alert-heading">ДЯКУЄМ, ЩО ВИБРАЛИ НАС!</h5>
+          <h5 className="alert-heading">ДЯКУЄМ, ЩО ВИБРАЛИ НАС!</h5>
           <span>Hаш менеджер обовязкова звяжеться з вами!</span>
         </>
       ) : (
         <>
-          <h5 class="alert-heading">Виникла помилка!</h5>
+          <h5 className="alert-heading">Виникла помилка!</h5>
           <span>Будь ласка споробуйте відправити ще раз!</span>
         </>
       )}
